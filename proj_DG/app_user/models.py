98827b54f3815c86da0a13250a3affae54026bc3
Ritesh Singh <rksinghr@gmail.com>
2025-09-01 21:48:53 +0530

    added customer refrence number to model

--- proj_DG/app_user/models.py
+++ proj_DG/app_user/models.py
@@ -4,9 +4,21 @@ from app_login.models import CustomUser
 class Profile(models.Model):
     user = models.OneToOneField(CustomUser, on_delete=models.CASCADE, related_name='profile')
     name = models.CharField(max_length=100)
+    customerRefNo = models.CharField(max_length=100, blank=True, null=True)
+    kycStatus = models.BooleanField(default=False)
+    nameProofType = models.CharField(max_length=100, blank=True, null=True)
+    addressProofType = models.CharField(max_length=100, blank=True, null=True)
+    nameProofId = models.CharField(max_length=100, blank=True, null=True)
+    addressProofId = models.CharField(max_length=100, blank=True, null=True)
+    dob = models.DateField(blank=True, null=True)
     billing_address = models.TextField()
     delivery_address = models.TextField()
     same_as_delivery = models.BooleanField(default=False)  # True if billing address is same as delivery
+
+    def save(self, *args, **kwargs):
+        if not self.customerRefNo and self.user_id:
+            self.customerRefNo = f"TS{str(self.user_id).zfill(8)}"
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.name
