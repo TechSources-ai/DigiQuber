eee6f9568b0f0d76380d1e0debdd87f96e5446a4
prabal1123 <136562395+prabal1123@users.noreply.github.com>
2026-01-12 13:49:33 +0530

    Comment out confirm email path and add confirm email OTP

--- proj_DG/app_login/urls.py
+++ proj_DG/app_login/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('create-password/', views.create_password_view, name='create_password'),
     path('complete-details/', views.complete_details_view, name='complete_details'),
     path('signin/', views.signin_view, name='signin'),
-    path('confirm-email/', views.confirm_email_view, name='confirm_email'),  # Add this line
+    #path('confirm-email/', views.confirm_email_view, name='confirm_email'),  # Add this line
     path('confirm-phone/', views.confirm_phone_view, name='confirm_phone'),  # If you use phone OTP
     path('logout/', views.logout_view, name='logout'),
     path("complete-profile/", views.complete_profile_view, name="complete_profile"),
-
+    path("confirm-email-otp/", confirm_email_otp_view, name="confirm_email_otp"),
 ]
